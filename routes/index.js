e21c14f4f014cb5f76a6c35789bfb76f2113c789
Hillary <h_strong69707@fanshaweonline.ca>
2019-10-21 21:07:26 -0400

    most updated class files

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,31 @@ const router = express.Router();
 
 const sql = require('../utils/sql');
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) => { // req stands for request, you are making a request on the site; res stands for response, when the server finds your stuff, it responds ot you
+
     // should really get the user data here and then fetch it thru, but let's try this asynchronously
     console.log('at the main route');
 
-    let query = "SELECT ID, avatar, Name, Logo, JobTitle FROM tbl_card";
+    let query = "SELECT ID, avatar, Name, Logo, JobTitle FROM tbl_card"; // have data that lives in a database, now give it to me on a page by going to retreive the data!!!
+
+    sql.query(query, (err, result) => {
+        if (err) { throw err; console.log(err); }
+
+        // console.log(result); // should see objects wrapped in an array
+
+        // render the home view with dynamic data
+        res.render('home', { people: result });
+    })
+})
+
+// this route is showing info to a specific person
+
+// this resolves to localhost:3000/anything (whatever you put after the slash in the location bar) --> add /anything to the browser and you should see that in the terminal in vsc
+router.get('/:id', (req, res) => {
+    console.log('hit a dynamic route!');
+    console.log(req.params.id);
+
+    let query = `SELECT * FROM tbl_bio WHERE profID="${req.params.id}"`;
 
     sql.query(query, (err, result) => {
         if (err) { throw err; console.log(err); }
@@ -15,7 +35,7 @@ router.get('/', (req, res) => {
         console.log(result); // should see objects wrapped in an array
 
         // render the home view with dynamic data
-        res.render('home', { data: result });
+        // res.render('home', { people: result });
     })
 })
 
